c6f15bd129fac062f7ea6ec9e5dd5b6eb27bd801
carlos gonzalez <carlos22gh@gmail.com>
2022-10-01 23:36:01 +0200

    Se traduce la página de contacto

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -2,7 +2,7 @@
   <div
     class="px-8 mx-auto mt-12 prose sm:px-6 md:px-4 lg:px-2 xl:px-0 xl:prose-xl lg:prose-lg md:prose-md"
   >
-    <h2>Contact</h2>
+    <h2>Contacta con nosotros</h2>
     <form
       name="contact"
       method="post"
@@ -13,7 +13,7 @@
     >
       <input type="hidden" name="form-name" value="contact" />
       <input class="hidden" name="bot-field" value="" />
-      <label class="label" for="name">Your name</label>
+      <label class="label" for="name">Nombre</label>
       <div class="control">
         <input
           id="name"
@@ -33,7 +33,7 @@
           required
         />
       </div>
-      <label class="label" htmlFor="message">Message</label>
+      <label class="label" htmlFor="message">Mensaje</label>
       <div class="control">
         <textarea
           id="message"
@@ -44,10 +44,10 @@
       </div>
       <div class="text-right">
         <button class="px-6 py-1 border border-gray-200 rounded" type="reset">
-          Cancel
+          Cancelar
         </button>
         <button class="px-6 py-1 text-white rounded bg-kaldi" type="submit">
-          Send
+          Enviar
         </button>
       </div>
     </form>
